95f2e001578e19e2e66722fd8c66a30b1cf3054c
Shumailseecs21 <114047978+Shumailseecs21@users.noreply.github.com>
2023-12-03 12:32:36 +0500

    creating search bar with styling

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Search Crawler</title>
+  <link rel="stylesheet" href="./styles/index.css">
+</head>
+<body>
+
+<div class="container">
+  <h1>Welcome to Search Crawler!</h1>
+  <h2>You can search anything</h2>
+  <form>
+    <input type="text" placeholder="Write any word to search">
+    <input type="submit" value="Search">
+  </form>
+</div>
+
+</body>
+</html>
